bb091d01446b8aed3594f84f1a3144ebf503d098
Julien Verley <mjules@gmail.com>
2025-06-13 11:39:13 +0200

    Meta modifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,59 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: "Julien Verley",
+	title: "Julien Verley | Développeur",
 	description:
-		"Portfolio de Julien Verley, développeur d'applications frontend ; Javascript, React, Next.js ; Mentorat et Services freelance",
+		"Portfolio de Julien Verley, développeur d'applications frontend Javascript React Next.js. Services freelance et mentorat pour vos projets web modernes.",
+	keywords: [
+		"développeur frontend",
+		"React",
+		"Next.js",
+		"JavaScript",
+		"TypeScript",
+		"freelance",
+		"mentorat",
+		"développement web",
+		"applications web",
+		"portfolio",
+		"Site web personnel",
+		"services web",
+		"création de sites web",
+		"création de sites internet",
+		"création d'applications",
+	],
+	authors: [{ name: "Julien Verley" }],
+	creator: "Julien Verley",
+	publisher: "Julien Verley",
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	openGraph: {
+		type: "website",
+		locale: "fr_FR",
+		url: "https://julienverley.vercel.app/",
+		title: "Julien Verley | Développeur Frontend JavsaScript React",
+		description:
+			"Portfolio de Julien Verley, développeur d'applications frontend Javascript React Next.js. Services freelance et mentorat.",
+		siteName: "Julien Verley Portfolio",
+		images: [
+			{
+				url: "/julien-photo-opengraph.png", // ajouter une image pour l'Open Graph
+				width: 1200,
+				height: 630,
+				alt: "Julien Verley - Développeur Frontend",
+			},
+		],
+	},
+	category: "technology",
+	classification: "Portfolio professionnel",
 };
 
 export default function RootLayout({
